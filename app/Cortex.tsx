d69ec1edeae14b4f9aea755d6014483d4ea7c5d0
Shubham Parihar <shubhamparihar391@gmail.com>
2023-09-26 02:46:09 +0530

    feat: add time and link

--- app/Cortex.tsx
+++ app/Cortex.tsx
@@ -7,16 +7,26 @@ export async function Cortex({ cortexRssFeed }: CortexParams) {
 	const feed = await parser.parseString(cortexRssFeed);
 
 	return (
-		<>
+		<div className='grid grid-cols-1 gap-y-2 px-4'>
 			{feed.items.map(item => {
 				return (
-					<div key={item.pubDate} className='mt-3'>
+					<div key={item.title} className='bg-gray-300 shadow-md px-4 py-2 rounded-md'>
 						<h1 className='font-bold'>{item.title}</h1>
-						<p>{item.pubDate}</p>
+						<p className='text-sm font-light'>
+							{new Date(Date.parse(item.pubDate!)).toLocaleString(undefined, {
+								weekday: 'short',
+								year: 'numeric',
+								month: 'long',
+								day: 'numeric',
+							})}
+						</p>
+						<a href={item.link} target='_blank' rel='noopener noreferrer' className='text-blue-700'>
+							{item.link}
+						</a>
 					</div>
 				);
 			})}
-		</>
+		</div>
 	);
 }
 
--- app/Defender.tsx
+++ app/Defender.tsx
@@ -7,16 +7,27 @@ export async function Defender({ defenderRssFeed }: DefenderParams) {
 	const feed = await parser.parseString(defenderRssFeed);
 
 	return (
-		<>
+		<div className='grid grid-cols-1 gap-y-2 px-4'>
 			{feed.items.map(item => {
 				return (
-					<div key={item.pubDate} className='mt-3'>
+					<div key={item.title} className='bg-gray-300 shadow-md px-4 py-2 rounded-md'>
 						<h1 className='font-bold'>{item.title}</h1>
-						<p>{item.pubDate}</p>
+						<p className='text-sm font-light'>
+							{new Date(Date.parse(item.pubDate!)).toLocaleString(undefined, {
+								weekday: 'short',
+								year: 'numeric',
+								month: 'long',
+								day: 'numeric',
+							})}
+						</p>
+						<p>{item.content}</p>
+						<a href={item.link} target='_blank' rel='noopener noreferrer' className='text-blue-700'>
+							{item.link}
+						</a>
 					</div>
 				);
 			})}
-		</>
+		</div>
 	);
 }
 
